0c596495a6a95041bc809dec032e14d23b85e80e
Michael Dugbartey <mikejemmanuel@gmail.com>
2024-01-22 15:36:43 -0800

    [0.53] GitPages New Load Method

--- NavBarHandler.js
+++ NavBarHandler.js
@@ -19,6 +19,6 @@ function setNavSize() {
     }
 }
 // Call the function on page load
-window.addEventListener('load', setNavSize);
+document.addEventListener('DOMContentLoaded', setNavSize);
 // Call the function when the window is resized
 window.addEventListener('resize', setNavSize);
