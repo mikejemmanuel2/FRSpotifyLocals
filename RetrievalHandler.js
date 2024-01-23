04ee5d14108cab5dd9412901cbafe521b567c734
Michael Dugbartey <mikejemmanuel@gmail.com>
2024-01-22 19:31:08 -0800

    [0.594] GitPages Song Retrieval Test 7

--- RetrievalHandler.js
+++ RetrievalHandler.js
@@ -1,7 +1,7 @@
 function setPlaylist() {
     var playlistName = new URLSearchParams(window.location.search).get('playlist');
     console.log(playlistName)
-    var dataLink = "http://nl2-2.deploy.sbs:2066/api/" + playlistName;
+    var dataLink = "https://nl2-2.deploy.sbs:2066/api/" + playlistName;
 
     const fetchData = async () => {
         const response = await fetch(dataLink);
