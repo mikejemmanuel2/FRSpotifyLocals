d5fff33739ca42d38b0920084a6103e9fbd1aef3
Michael Dugbartey <mikejemmanuel@gmail.com>
2024-01-23 02:59:30 -0800

    [0.5997] GitPages Song Retrieval Test 19

--- RetrievalHandler.js
+++ RetrievalHandler.js
@@ -3,7 +3,6 @@ function setPlaylist() {
     var playlistName = new URLSearchParams(window.location.search).get('playlist');
     console.log(playlistName)
     var dataLink = "https://mikejemmanuel.pythonanywhere.com/api/" + playlistName;
-    socket = io.connect(dataLink);
 
     const fetchData = async () => {
         const response = await fetch(dataLink);
@@ -11,12 +10,6 @@ function setPlaylist() {
     console.log("Starting Fetch")
     fetchData()
 
-    socket.on('message', function(data) {
-        console.log('Received message:', data.data);
-    });
-    socket.on('update', function(data) {
-        console.log('Received update:', data.message);
-    });
 }
 
 // Call the function on page load
